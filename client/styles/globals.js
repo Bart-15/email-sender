460ef126613dbb4afdf24610d27b6edef7d6a5bf
Bart-15 <brat.tabusao@gmail.com>
2022-05-23 22:35:32 +0800

    fixed: UI responsiveness

---
+++ client/styles/globals.js
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+const GlobalStyles = createGlobalStyle`
+  ${normalize};
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+  html {
+    font-size: 62.5%;
+    scroll-behavior: smooth;
+  }
+  body {
+    font-family: ${props => props.theme.fonts.main};
+    font-size: 1.6rem;
+    background: ${props => props.theme.colors.background1};
+    background-image: 
+      radial-gradient(at 47% 33%, hsl(227.21, 79%, 48%) 0, transparent 59%), 
+      radial-gradient(at 82% 65%, hsl(190.59, 0%, 100%) 0, transparent 55%);
+    color: ${props => props.theme.colors.primary1};
+    cursor: default;
+  }
+  h1,h2,h3,h4,h5,h6,button {
+    font-family: ${props => props.theme.fonts.title};
+  }
+  a {
+    text-decoration: none;
+  }
+  li{
+    list-style: none;
+  }
+`;
+
+export default GlobalStyles;
---
+++ client/themes/default.js
@@ -0,0 +1,32 @@
+export default {
+    // Temp fonts
+    fonts: {
+      title: "Space Grotesk, sans-serif",
+      main: "Space Grotesk, sans-serif"
+    },
+    // fontsize
+    fontSize : {
+        sm:"12px",
+        md:"14px",
+        lg:"16px"
+    },
+
+    // Colors for layout
+    colors: {
+      error:"#e05634",
+      primary1: "hsl(204,23.8%,95.9%)",
+      background1: "#cbe1e1",
+      accent1: "#4b5663",
+      button: "hsl(205.1,100%,36.1%)",
+      background2: "hsl(232.7,27.3%,23.7%)",
+      success:"#2ea44f"
+    },
+    // Breakpoints for responsive design
+    breakpoints: {
+      xs: 'screen and (min-width: 375px)',
+      sm: 'screen and (min-width: 640px)',
+      md: 'screen and (min-width: 768px)',
+      lg: 'screen and (min-width: 1024px)',
+      xl: 'screen and (min-width: 1280px)'
+    },
+  }
